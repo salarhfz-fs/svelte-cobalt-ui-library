464508719c1739110e9b265eef5568f22d675a7f
Salar Hafezi <salar.hfz.work@gmail.com>
2023-05-28 20:17:26 +0330

    Add Input component (v1)

---
+++ src/lib/Input/Input.stories.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+    import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+
+    import Input from './Input.svelte';
+</script>
+
+<Meta
+    title='Cobalt UI Library/Basic Components/Input'
+    component={Input}
+/>
+
+<Template let:args>
+    <Input {...args} />
+</Template>
+
+<Story
+    name='Default'
+    args={
+        { placeholder: 'example test', label: 'Text:', onInput: () => alert('this is a text field')  }
+    }
+/>
+
+<Story
+    name='Email'
+    args={
+        {
+            placeholder: 'email@example.com',
+            label: 'Email:',
+            fieldType: 'email',
+            onInput: () => alert('this is an email field')
+        }
+    }
+/>
+
+<Story
+    name='No label'
+    args={
+        {
+            placeholder: 'no label',
+            label: '',
+            fieldType: 'text',
+            onInput: () => alert('this is an field with no label') }
+    }
+/>
+
+<Story
+    name='Disabled'
+    args={
+        { placeholder: 'disabled field', label: 'Text:', fieldType: 'text', disabled: true }
+    }
+/>
+
